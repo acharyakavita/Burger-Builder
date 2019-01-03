58d68c7acc441fd19f3a5224b91a024f26d32ab1
acharyakavita <acharyakavita12@gmail.com>
2019-01-03 17:25:33 -0500

    Added Build Controls layout and control buttons

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,21 +1,21 @@
 import React,{Component} from 'react';
 import Aux from '../../hoc/Auxilary';
-import Burger from '../../Components/Burger/Burger'
-
+import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
     state={
         ingredients:{
-            salad:0,
-            bacon:0,
-            cheese:0,
-            meat:0,
+            salad:1,
+            bacon:1,
+            cheese:3,
+            meat:1,
         }
     }
     render(){
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         )
     }
