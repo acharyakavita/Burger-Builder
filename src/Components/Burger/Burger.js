8d8a2632bc17ee2da31bfddfa5aae172f1d9f25d
acharyakavita <acharyakavita12@gmail.com>
2019-01-03 16:04:41 -0500

    Generic dynamic burger builder layout

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,13 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+    /*The below code gets an object as input from the props having ingredients name and quantity.
+    We use Object.keys to convert the recieved object into an array of elements with the repeatition of ingredients as per the quantity*/
+    const arrayOfIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) =>
+                <BurgerIngredients key={igKey + i} type={igKey} />
+            )
+        })
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
-            <BurgerIngredients type="salad" />
-            <BurgerIngredients type="bacon" />
-            <BurgerIngredients type="cheese" />
-            <BurgerIngredients type="meat" />
+            {arrayOfIngredients}
             <BurgerIngredients type="bread-bottom" />
         </div>
     )
