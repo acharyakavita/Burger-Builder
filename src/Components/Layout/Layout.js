fb8433018ef9589f39d252a3a8e372baa677985d
acharyakavita <acharyakavita12@gmail.com>
2019-01-04 16:58:31 -0500

    Added static SideDrawer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/Auxilary';
 import Classes from '../Layout/Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = (props) => (
     <Aux>
+        <SideDrawer/>
         <Toolbar/>
         <main className={Classes.Content}>
             {props.children}
