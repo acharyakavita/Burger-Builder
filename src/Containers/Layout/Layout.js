50f66f4f2d54d3d1bef1e9a1d84079279d5d39ac
acharyakavita <acharyakavita12@gmail.com>
2019-01-04 19:46:04 -0500

    performance tuning

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilary';
-import Classes from '../Layout/Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Classes from './Layout.css';
+import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../Components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
